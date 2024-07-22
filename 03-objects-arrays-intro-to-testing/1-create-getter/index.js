5b6efe16c5734b1153a1897b8005c4ff72239f24
bulatenkom <bulatenkom@gmail.com>
2024-07-22 16:16:48 +0700

    m3t1: add createGetter func

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const keys = path.split('.');
 
+    return function(obj) {
+        let value = obj;
+        for (const key of keys) {
+            if (!value.hasOwnProperty(key)) return;
+            value = value[key]
+        }
+        return value;
+    }
 }
