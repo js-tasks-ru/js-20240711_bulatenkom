af2b8b3ea3657c5076bc1530c4aa839cbde259bb
bulatenkom <bulatenkom@gmail.com>
2024-07-29 21:13:22 +0700

    m5t1: add NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,52 @@
 export default class NotificationMessage {
+    static lastShownComponent;
+    element;
+    timer;
 
+    constructor(msg = '', { type = 'success', duration = 1500 } = {}) {
+        this.msg = msg;
+        this.type = type;
+        this.duration = duration;
+
+        this.element = this.createElement(this.createNotificationTemplate());
+    }
+
+    show(target = document.body) {
+        if (NotificationMessage.lastShownComponent) {
+            NotificationMessage.lastShownComponent.destroy();
+        }
+        
+        NotificationMessage.lastShownComponent = this;
+        this.timer = setTimeout(() => {
+            this.destroy();
+        }, this.duration);
+        target.append(this.element);
+    }
+
+    createElement(template) {
+        const element = document.createElement('div');
+        element.innerHTML = template;
+        return element.firstElementChild;
+    }
+
+    createNotificationTemplate() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.duration}ms">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">${this.msg}</div>
+                </div>
+            </div>
+        `
+    }
+
+    remove() {
+        clearTimeout(this.timer);
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 }
