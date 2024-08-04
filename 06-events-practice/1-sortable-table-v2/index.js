b80f245e38849be1bd0aaf0293a9072231e154f3
bulatenkom <bulatenkom@gmail.com>
2024-08-05 06:34:02 +0700

    add destroy method to component

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,6 +1,8 @@
 import {default as SortableTableV1} from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
 export default class SortableTable extends SortableTableV1 {
+  #handlers = new Map();
+
   constructor(headersConfig = [], {
     data = [],
     sorted = {}
@@ -8,7 +10,7 @@ export default class SortableTable extends SortableTableV1 {
     super(headersConfig, data);
     this.sort(sorted.id, sorted.order);
 
-    this.subElements['header'].addEventListener('pointerdown', this.onHeaderClick.bind(this));
+    this.addEventListener(this.subElements['header'], 'pointerdown', this.onHeaderClick);
   }
 
   onHeaderClick(event) {
@@ -20,5 +22,25 @@ export default class SortableTable extends SortableTableV1 {
       this.sort(id, order);
       sortableElement.append(arrowElement);
     }
+  }
+
+  addEventListener(element, eventType, handler) {
+    const h = handler.bind(this);
+    element.addEventListener(eventType, h)
+    this.#handlers.set(h, {
+      element,
+      eventType
+    });
+  }
+
+  removeEventListeners() {
+    for (const [handler, { element, eventType }] of this.#handlers) {
+      element.removeEventListener(eventType, handler);
+    }
+  }
+
+  destroy() {
+    this.removeEventListeners();
+    super.destroy();
   }
 }
