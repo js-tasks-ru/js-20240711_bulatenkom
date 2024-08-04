760eed81717a4cc76fcb485715f957563b6c86e2
bulatenkom <bulatenkom@gmail.com>
2024-08-05 04:47:05 +0700

    m6t1: add SortableTable v2

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,24 @@
-export default class SortableTable {
-  constructor(headersConfig, {
+import {default as SortableTableV1} from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
+
+export default class SortableTable extends SortableTableV1 {
+  constructor(headersConfig = [], {
     data = [],
     sorted = {}
   } = {}) {
+    super(headersConfig, data);
+    this.sort(sorted.id, sorted.order);
 
+    this.subElements['header'].addEventListener('pointerdown', this.onHeaderClick.bind(this));
+  }
+
+  onHeaderClick(event) {
+    const sortableElement = event.target.closest('[data-sortable=true]')
+    if (sortableElement) {
+      const order = sortableElement.dataset.order === 'asc' ? 'desc' : 'asc';
+      const id = sortableElement.dataset.id;
+      const arrowElement = this.subElements['arrow'];
+      this.sort(id, order);
+      sortableElement.append(arrowElement);
+    }
   }
 }
